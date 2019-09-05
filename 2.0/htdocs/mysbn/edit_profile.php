5395848e37e7cf8672f1c6091001cee6d71736d4
root <root@localhost.localdomain>
2019-09-05 13:46:46 +0530

    autocmplt is cmplt

--- 2.0/htdocs/mysbn/edit_profile.php
+++ 2.0/htdocs/mysbn/edit_profile.php
@@ -18,6 +18,9 @@ $sitesession->Session();
     <title>Edit your profile : SBN</title>
     <? include("includes/style.php"); ?>
   </head>
+
+  <!-- try 1.0 -->
+  <!-- TypeError: Illegal constructor
   <script src="https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places&key=AIzaSyBnzedToDdeq9Ax0F2DyjmUsxyG0GdeLF0">
   </script>
   <script>
@@ -28,7 +31,35 @@ $sitesession->Session();
       console.log(place.url);
       console.log(place.geometry.location);
     });
+  </script> -->
+
+  <!-- try 2.0 -->
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBnzedToDdeq9Ax0F2DyjmUsxyG0GdeLF0"></script>
+  <script type="text/javascript">
+    function initialize() {
+      var input = document.getElementById('location');
+      var options = {
+        types: ['address'],
+        // componentRestrictions: {
+        //   country: 'us'
+        // }
+      };
+      autocomplete = new google.maps.places.Autocomplete(input, options);
+      autocomplete = new google.maps.places.Autocomplete(input, options);
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          var place = autocomplete.getPlace();
+          for (var i = 0; i < place.address_components.length; i++) {
+            for (var j = 0; j < place.address_components[i].types.length; j++) {
+              if (place.address_components[i].types[j] == "postal_code") {
+                document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+              }
+            }
+          }
+        })
+      }//initializeEnds
+      google.maps.event.addDomListener(window, "load", initialize);
   </script>
+
   
   <body>
     <? include("includes/header.php"); ?>
@@ -80,7 +111,7 @@ $sitesession->Session();
                     </tr>
                     <tr>
                       <th scope="col"><label>Location</label></th>
-                      <th scope="col"><textarea type="text" name="location" id="location" class="form-control" placeholder="Location" value="" required></textarea></th>
+                      <th scope="col"><input type="text" name="location" id="location" class="form-control" placeholder="Location" value="" required></th>
                       <th scope="col"></th>
                     </tr>
                     <tr>
